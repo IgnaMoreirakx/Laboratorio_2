860c9936ce7b6711c2616534f0e4aac4c7baab09
Gaspar Catalàn <gaspar.catalan@usach.cl>
2023-07-11 21:23:14 -0400

    [Gaspar Catalán] Implementación de Calculo de promedios de Habilidades

--- lab2-backend/src/main/java/grupo1/lab2/Controllers/VoluntarioController.java
+++ lab2-backend/src/main/java/grupo1/lab2/Controllers/VoluntarioController.java
@@ -3,7 +3,9 @@ package grupo1.lab2.Controllers;
 import grupo1.lab2.Documents.VoluntarioDocument;
 import grupo1.lab2.Services.VoluntarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,11 @@ public class VoluntarioController {
     @GetMapping
     public List<VoluntarioDocument> getAll(){
         return voluntarioService.getAll();
+    }
+
+    @GetMapping("/{id}/averageSkills")
+    public ResponseEntity<Double> obtenerPromedioHabilidades(@PathVariable String id) {
+        double average = voluntarioService.calcularPromedioHabilidades(id);
+        return ResponseEntity.ok(average);
     }
 }
--- lab2-backend/src/main/java/grupo1/lab2/Services/VoluntarioService.java
+++ lab2-backend/src/main/java/grupo1/lab2/Services/VoluntarioService.java
@@ -1,5 +1,6 @@
 package grupo1.lab2.Services;
 
+import grupo1.lab2.Documents.HabilidadDocument;
 import grupo1.lab2.Documents.VoluntarioDocument;
 import grupo1.lab2.Repository.VoluntarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,5 +14,11 @@ public class VoluntarioService {
     VoluntarioRepository voluntarioRepository;
     public List<VoluntarioDocument> getAll(){
         return voluntarioRepository.findAll();
+    }
+
+    public double calcularPromedioHabilidades(String id) {
+        VoluntarioDocument voluntario = voluntarioRepository.findById(id);
+        return voluntario.getHabilidades().stream().mapToInt(HabilidadDocument::getPuntaje).average().orElse(0.0);
+
     }
 }
